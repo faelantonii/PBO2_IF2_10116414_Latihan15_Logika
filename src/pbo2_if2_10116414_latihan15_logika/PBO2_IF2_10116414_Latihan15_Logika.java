0517d34ecd2d9d1199d247de9f912f98a9a4e67d
sweenutt <sweenutt@192.168.3.19>
2020-10-20 02:23:13 +0700

    Latihan 15 Program Proses Logika

---
+++ src/pbo2_if2_10116414_latihan15_logika/PBO2_IF2_10116414_Latihan15_Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2_if2_10116414_latihan15_logika;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * Deskripsi    : Program Logika
+ */
+public class PBO2_IF2_10116414_Latihan15_Logika {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(" a && b \t= " + (a && b));
+        System.out.println(" a || b \t= " + (a && b));
+        System.out.println(" !a \t\t= " + !a);
+        System.out.println(" !(a && b) \t= " + (a && b));
+    }
+}
